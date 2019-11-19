eee2d5e490aa1d909e5e22d21f5ae040877e6331
Jan Repnak <io@jrx.de>
2019-11-19 19:25:54 +0100

    scrabble

---
+++ go/scrabble-score/scrabble_score.go
@@ -0,0 +1,30 @@
+package scrabble
+
+import "strings"
+
+// Score computes the Scrabble score for a given word
+func Score(word string) int {
+
+	score := 0
+
+	for _, r := range strings.ToUpper(word) {
+		switch r {
+		case 'A', 'E', 'I', 'O', 'U', 'L', 'N', 'R', 'S', 'T':
+			score++
+		case 'D', 'G':
+			score += 2
+		case 'B', 'C', 'M', 'P':
+			score += 3
+		case 'F', 'H', 'V', 'W', 'Y':
+			score += 4
+		case 'K':
+			score += 5
+		case 'J', 'X':
+			score += 8
+		case 'Q', 'Z':
+			score += 10
+		}
+	}
+
+	return score
+}
